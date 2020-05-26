99edbf75cf4c8e4247e95bd1cf2c20c30486e757
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-05-26 11:25:52 +0200

    Add files via upload

---
+++ svanTipuricZapisivanjeUTekstualniDokument/svanTipuricZapisivanjeUTekstualniDokument/StreamWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricZapisivanjeUTekstualniDokument
+{
+    class StreamWriter
+    {
+        using (StreamWriter writer = new StreamWriter(fullPath))  
+        {  
+         writer.WriteLine("Monica Rathbun");  
+         writer.WriteLine("Vidya Agarwal");  
+         writer.WriteLine("Mahesh Chand");  
+         writer.WriteLine("Vijay Anand");  
+         writer.WriteLine("Jignesh Trivedi");  
+        }
+        
+          string readText = File.ReadAllText(fullPath);
+          Console.WriteLine(readText);
+    
+    }
+}
